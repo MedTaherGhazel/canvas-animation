f0439955e0a79d4bcea70d65e01e6377a2dae610
Med Taher Ghazel <119009624+MedTaherGhazel@users.noreply.github.com>
2024-01-10 12:01:07 +0100

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -154,3 +154,6 @@
       this.ctx.stroke();
     }
   }
+ getDistance(p1: { x: number; y: number; }, p2: { x: number; y: number; }) {
+    return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+  }
