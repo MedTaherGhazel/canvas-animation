f7c5f7a2aebc322d0ff24855bca2da997f03290f
Med Taher Ghazel <119009624+MedTaherGhazel@users.noreply.github.com>
2024-01-10 12:03:40 +0100

    Update and rename canvas.js to canvas.ts

--- canvas.ts
+++ canvas.ts
@@ -174,3 +174,6 @@ class Circle {
     this.ctx.fill();
   }
 }
+ getDistance(p1: { x: number; y: number; }, p2: { x: number; y: number; }) {
+    return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+  }
